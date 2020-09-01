970754e0507a67b51300d1a5f673cdbd71d61075
Vitamin-JS <Vpopchenkov@gmail.com>
2020-09-02 01:16:44 +0300

    Add условие задачи 5-5

--- js/homework05.js
+++ js/homework05.js
@@ -175,48 +175,200 @@
 //
 
 // Write code under this line
-class StringBuilder {
-  constructor(string) {
-    this._value = string;
+// class StringBuilder {
+//   constructor(string) {
+//     this._value = string;
+//   }
+
+//   get value() {
+//     return this._value;
+//   }
+
+//   set value(newValue) {
+//     this._value = newValue;
+//   }
+
+//   append(str) {
+//     return (this._value = [this._value] + str);
+//   }
+
+//   prepend(str) {
+//     return (this._value = str + [this._value]);
+//   }
+
+//   pad(str) {
+//     this.append(str);
+//     this.prepend(str);
+//     return this._value;
+//   }
+// }
+
+// // -----------------------------------------------------------------------------------------------------
+// //
+
+// console.log(typeof StringBuilder);
+// // 'function'  ------ OK
+
+// const builder = new StringBuilder(".");
+// console.log(builder._value); // '.'   ---------- OK
+
+// builder.append("^");
+// console.log(builder.value); // '.^' ------------ OK
+
+// builder.prepend("^");
+// console.log(builder.value); // '^.^' ----------- OK
+
+// builder.pad("=");
+// console.log(builder.value); // '=^.^=' --------- OK
+
+// =====================================   Задача 5-5   ================================================
+// =====================================================================================================
+//
+  КЛАСС
+Напиши класс Car с указанными свойствами и методами.
+
+class Car {
+    /*
+     * Добавь `статический` метод 
+     * `getSpecs(car)`, который принимает 
+     * объект-машину как параметр 
+     * и возвращает шаблонную строку 
+     * со свойствами и значениями объекта.
+     * Свойства: 
+     *   maxSpeed, 
+     *   speed, 
+     *   isOn, 
+     *   distance,
+     *   price
+     * Пример строки, полученной этим методом:
+     * 'maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000'
+     */
+
+    /*
+     * Конструктор получает объект настроек.
+     *
+     * Добавь свойства будущеего экземпляра класса:
+     *  speed - текущая скорость, 
+     *          начальное значение `0`
+     *  price - цена автомобиля
+     *  maxSpeed - максимальная скорость
+     *  isOn - заведен ли автомобиль. 
+     *         Значения `true` или `false`,  
+     *         начальное значение false
+     *  distance - пробег в километрах, 
+     *             начальное значение `0`
+     */
+    constructor() { }
+
+    /*
+     * Добавь геттер и сеттер 
+     * для свойства `price`, который будет 
+     * работать с свойством цены автомобиля.
+     * 
+     * ВАЖНО: для записи методов get и set
+     * значение параметра записывают с 
+     * подчеркиванием. См. ниже пример 1.
+     */
+
+    /*
+     * Метод, который заводит автомобиль
+     * Записывает в свойство `isOn` значение `true`
+     */
+    turnOn() { }
+
+    /*
+     * Метод, чтобы заглушить автомобиль
+     * Этот метод должен записывать 
+     * в свойство isOn значение false,
+     * и сбрасывать текущую скорость до 0
+     */
+    turnOff() { }
+
+    /*
+     * Этот метод должен добавлять 
+     * к свойству `speed` полученное
+     * значение, при условии, 
+     * что результирующая скорость
+     * не больше чем значение свойства `maxSpeed`
+     */
+    accelerate(value) { }
+
+    /*
+     * Этот метод должен отнимать 
+     * от свойства `speed` 
+     * полученное значение, при условии, 
+     * что результирующая скорость не меньше 0
+     */
+
+    decelerate(value) { }
+
+    /*
+     * Этот метод должен добавлять к свойству 
+     * `distance` пробег в километрах, 
+     * т.е. hours * speed,
+     * но только в том случае, 
+     * если машина заведена!
+     */
+    drive(hours) { }
+}
+
+#### Пример 1.
+
+    ```js
+class Guest {
+  // Собственные свойства класса размещаем в конструкторе
+  constructor(name, roomNumber) {
+    /* параметр name не должен 
+     *   совпадать с названием свойства
+     *   но должен быть похож.
+     *   Поэтому используют прием смены
+     *   названия добавляя символ '_'.
+     *   Хотя это может любой другой символ
+     */
+    this._name = name;
   }
 
-  get value() {
-    return this._value;
+  // Используем геттеры и сеттеры для описания интерфейса доступа к свойствам
+  get name() {
+    return this._name;
   }
 
-  set value(newValue) {
-    this._value = newValue;
-  }
-
-  append(str) {
-    return (this._value = [this._value] + str);
-  }
-
-  prepend(str) {
-    return (this._value = str + [this._value]);
-  }
-
-  pad(str) {
-    this.append(str);
-    this.prepend(str);
-    return this._value;
+  set name(value) {
+    this._name = value;
   }
 }
 
-// -----------------------------------------------------------------------------------------------------
+// -------------------------------------   Решение   ---------------------------------------------------
 //
 
-console.log(typeof StringBuilder);
-// 'function'  ------ OK
+class Car {
+ // Write code under this line
+  getSpecs(){}
+  constructor(){}
+  get price() {}
+  set price(value) {}
+  turnOn() {}
+  turnOff() {}
+  accelerate(value) {}
+  decelerate(value) {}
+  drive(hours) {}
+}
+ 
+const mustang = new Car({ maxSpeed: 200, price: 2000 });
+mustang.turnOn();
+mustang.accelerate(50);
+mustang.drive(2);
 
-const builder = new StringBuilder(".");
-console.log(builder._value); // '.'   ---------- OK
+// console.log(Car.getSpecs(mustang));
+// 'maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000'
 
-builder.append("^");
-console.log(builder.value); // '.^' ------------ OK
+mustang.decelerate(20);
+mustang.drive(1);
+mustang.turnOff();
 
-builder.prepend("^");
-console.log(builder.value); // '^.^' ----------- OK
+// console.log(Car.getSpecs(mustang));
+// 'maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000'
 
-builder.pad("=");
-console.log(builder.value); // '=^.^=' --------- OK
+// console.log(mustang.price); // 2000
+mustang.price = 4000;
+// console.log(mustang.price); // 4000
